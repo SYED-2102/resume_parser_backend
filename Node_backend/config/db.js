29e1ff5fda1472e0f34d135c8b27f63dfd3b124f
pusasaisanjay123-wq <pusasaisanjay123@gmail.com>
2026-04-23 12:09:02 +0530

    deleted extra lines for improving readability

--- Node_backend/config/db.js
+++ Node_backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log("✅ MongoDB connected");
+    console.log(" MongoDB connected");
   } catch (error) {
-    console.error("❌ DB connection error:", error.message);
+    console.error(" DB connection error:", error.message);
     process.exit(1);
   }
 };
--- Node_backend/middleware/multer.js
+++ Node_backend/middleware/multer.js
@@ -5,9 +5,7 @@ const storage = multer.memoryStorage();
 
 export const upload = multer({storage:
   storage,
-//   limits: {
-//     fileSize: 10 * 1024 * 1024, // 10MB
-//   },
+
 });
 
 // Field config (reusable)
