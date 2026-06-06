2b8cbc41bba5210b763e89de363ab83a39778971
pusasaisanjay123-wq <pusasaisanjay123@gmail.com>
2026-06-06 21:40:00 +0530

    remove React_frontend folder

--- React_frontend/src/index.css
+++
@@ -1,8 +0,0 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-* { box-sizing: border-box; }
-body { font-family: "Syne", sans-serif; margin: 0; }
-::-webkit-scrollbar { width: 4px; }
-::-webkit-scrollbar-track { background: #0f172a; }
-::-webkit-scrollbar-thumb { background: #334155; border-radius: 2px; }
--- React_frontend/src/services/api.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-const API = axios.create({ baseURL: "", timeout: 180000 });
-API.interceptors.response.use(r => r, err => { console.error("API:", err.response?.data || err.message); return Promise.reject(err); });
-export default API;
